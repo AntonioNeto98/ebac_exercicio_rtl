a73397bee53f2f4c56dddd6da2005c79c7ff64ed
AntonioNeto98 <141379093+AntonioNeto98@users.noreply.github.com>
2024-08-22 07:59:05 -0300

    novo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://i.pinimg.com/564x/75/6c/8a/756c8a1f5ee4eeaa728f8b83067cda5e.jpg">
+      <Post imageUrl="https://i.pinimg.com/564x/75/6c/8a/756c8a1f5ee4eeaa728f8b83067cda5e.jpg" >
         Olha só que legal minha miniatura do Batwing.
       </Post>
     </div>
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,26 +1,24 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '../Post';
 import PostComment from '../PostComments';
+import test from 'node:test';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
 
-    it('Deve permitir a inserção de dois comentários', () => {
+    test('Deve permitir a inserção de dois comentários', () => {
         render(<Post children={undefined} imageUrl={''} />);
-        
         
         fireEvent.change(screen.getByTestId('comment-input'), { target: { value: 'Primeiro comentário' } });
         fireEvent.click(screen.getByTestId('submit-button'));
         
         expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
         
-        
         fireEvent.change(screen.getByTestId('comment-input'), { target: { value: 'Segundo comentário' } });
         fireEvent.click(screen.getByTestId('submit-button'));
-        
         
         expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
     });
